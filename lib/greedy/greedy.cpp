a73d14540c3f6ad80894b647c87810471eb6bbcd
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-03-17 15:27:10 -0300

    normal schedule done

--- lib/greedy/greedy.cpp
+++ lib/greedy/greedy.cpp
@@ -1,26 +1,61 @@
 #include "greedy.hpp"
 #include <algorithm>
-#include <map>
 
 Greedy::Greedy()
 {
+    this->categoriesCount = new std::vector<int>(*(this->categories), 0);
+    this->acceptedMovies = new Movie *[24];
+    this->acceptedMoviesCount = new int;
+    *(this->acceptedMoviesCount) = 0;
 }
 
 Greedy::~Greedy()
 {
+    delete this->categoriesCount;
+    for (int i = 0; i < 24; i++)
+        delete this->acceptedMovies[i];
+    delete[] this->acceptedMovies;
+
+    delete this->agenda;
+    delete this->movieSchedule;
+    delete this->acceptedMoviesCount;
 }
 
-void Greedy::schedule()
+void Greedy::normalSchedule()
 {
-    for (int i = 0; i < *(this->movies); i++)
-        this->intervals.push_back(Interval<int, Movie *>(this->moviesList[i]->startTime, this->moviesList[i]->endTime, this->moviesList[i]));
+    std::sort(this->moviesList, this->moviesList + *(this->movies), [](Movie *a, Movie *b)
+              { return a->endTime < b->endTime; });
 
-    this->intervalTree = IntervalTree<int, Movie *>(this->intervals);
+    this->agenda = new std::vector<bool>(24, 0);
+    this->movieSchedule = new std::vector<bool>(24, 0);
+    for (int i = 0; i < *(this->movies); i++)
+    {
+        if (this->moviesList[i]->startTime < this->moviesList[i]->endTime && this->moviesList[i]->startTime >= 0 && this->moviesList[i]->endTime < 24)
+        {
+            for (int j = this->moviesList[i]->startTime; j < this->moviesList[i]->endTime; j++)
+                this->movieSchedule->at(j) = 1;
+
+            if (std::equal(this->agenda->begin(), this->agenda->end(), this->movieSchedule->begin(), [](bool a, bool b)
+                           { return !(a & b); }) &&
+                this->categoriesCount->at(this->moviesList[i]->category - 1) <= *(this->maxMoviesPerCat[this->moviesList[i]->category - 1]))
+            {
+                this->acceptedMovies[++*(this->acceptedMoviesCount)] = this->moviesList[i];
+                this->categoriesCount->at(this->moviesList[i]->category - 1)++;
+            }
+        }
+    }
+}
+
+void Greedy::optimizedSchedule()
+{
+    // for (int i = 0; i < *(this->movies); i++)
+    //     this->intervals.push_back(Interval<int, int>(this->moviesList[i]->startTime, this->moviesList[i]->endTime, this->moviesList[i]));
+
+    // this->intervalTree = IntervalTree<int, int>(this->intervals);
 }
 
 void Greedy::showAcceptedMovies()
 {
-    int i = -1;
-    while (this->acceptedMovies[++i] != nullptr)
+    for (int i = 0; i < *(this->acceptedMoviesCount); i++)
         std::cout << this->acceptedMovies[i]->category << ' ' << this->acceptedMovies[i]->startTime << ' ' << this->acceptedMovies[i]->endTime << '\n';
 }
--- lib/greedy/greedy.hpp
+++ lib/greedy/greedy.hpp
@@ -3,17 +3,25 @@
 #include "scheduler/scheduler.hpp"
 #include "IntervalTree.h"
 #include <vector>
+#include <map>
 
 class Greedy : public Scheduler
 {
 private:
-    std::vector<Interval<int, Movie *>> intervals;
-    IntervalTree<int, Movie *> intervalTree;
     Movie **acceptedMovies;
+    int *acceptedMoviesCount;
+
+    std::vector<bool> *agenda;
+    std::vector<bool> *movieSchedule;
+    std::vector<int> *categoriesCount;
+
+    std::vector<Interval<int, int>> intervals;
+    IntervalTree<int, int> intervalTree;
 
 public:
     Greedy();
     ~Greedy();
-    void schedule();
+    void normalSchedule();
+    void optimizedSchedule();
     void showAcceptedMovies();
 };
