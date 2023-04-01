ba240b96932b2131180b6c7a3c6ebb899f2b43ec
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-04-01 11:17:27 -0300

    input e output generator

--- scripts/input_generator.bash
+++ scripts/input_generator.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ ! -d "in" ]; then
-    mkdir in
-fi
-
-for i in {1..500}; do
-    num_movies=$(( $RANDOM % 9999991 + 10 ))
-    num_categories=$(( $RANDOM % 46 + 5 ))
+for i in {1..250}; do
+    num_movies=$(( $i * 4000 ))
+    num_categories=100
     filename="input_$i.txt"
-    ../bin $num_movies $num_categories $filename 0
+    ../bin/generator $num_movies $num_categories $filename 0
+done
+
+for i in {251..500}; do
+    num_movies=10000
+    num_categories=$(( ($i - 250) * 4 ))
+    filename="input_$i.txt"
+    ../bin/generator $num_movies $num_categories $filename 0
 done
--- scripts/output_generator.bash
+++ scripts/output_generator.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "out" ]; then
-    mkdir out
-    mkdir out/greedy
-    mkdir out/random
-fi
+# mkdir -p ../out/greedy
+# mkdir -p ../out/random
+
+# for i in {1..500}; do
+#     filename="input_$i.txt"
+#     touch "../out/greedy/$filename"
+#     touch "../out/random/$filename"
+# done
 
 for file in ../in/*.txt
 do
     filename=$(basename -- "${file%.*}")
-    ../bin/main < "$file" > "../out/greedy/$filename.txt"
-    ../bin/main < "$file" > "../out/random/$filename.txt"
+    ../bin/main 0 < "$file" > "../out/greedy/$filename.txt"
+    ../bin/main 1 < "$file" > "../out/random/$filename.txt"
 done
