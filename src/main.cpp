063136af4cafbb50e9ac54ee28b878d5dc3d4b4a
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-04-01 11:15:10 -0300

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include "greedy/greedy.hpp"
 #include "random/random.hpp"
+#include <chrono>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-    if (argv[3][0] == '1')
+    if (argv[1][0] == '1')
     {
         Random random = Random();
         random.readMoviesCatalog();
+
+        // start timer here
+        auto start = std::chrono::high_resolution_clock::now();
         random.scheduleMovies();
-        random.showAcceptedMovies();
+        auto finish = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = finish - start;
+        std::cout << 0 << ' ' << *random.movies << ' ' << *random.categories << ' ' << *random.acceptedMoviesCount << ' ' << elapsed.count() << '\n';
+
+        // random.showAcceptedMovies();
     }
-    else if (argv[3][0] == '0')
+    else if (argv[1][0] == '0')
     {
         Greedy greedy = Greedy();
         greedy.readMoviesCatalog();
+
+        // start timer here
+        auto start = std::chrono::high_resolution_clock::now();
         greedy.scheduleMovies();
-        greedy.showAcceptedMovies();
+        auto finish = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = finish - start;
+        std::cout << 0 << ' ' << *greedy.movies << ' ' << *greedy.categories << ' ' << *greedy.acceptedMoviesCount << ' ' << elapsed.count() << '\n';
+        // greedy.showAcceptedMovies();
     }
     return 0;
 }
