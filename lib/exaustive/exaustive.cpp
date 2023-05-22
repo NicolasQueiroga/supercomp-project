477dc6710d2ef242fae59a4494b082b2ffce5f25
NicolasQueiroga <n.macielqueiroga@gmail.com>
2023-05-22 23:02:25 +0000

    update

---
+++ lib/exaustive/exaustive.cpp
@@ -0,0 +1,2 @@
+#include "exaustive.hpp"
+
---
+++ lib/exaustive/exaustive.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "utils.hpp"
+#include "scheduler/scheduler.hpp"
+#include <bitset>
+#include <memory>
+
+    
+class Exaustive : public Scheduler
+{
+private:
+    
+};
